3f2d099b54265f1df30bd9bb08b4977b615a9504
hettipeti <git@thepeko.at>
2018-09-02 19:27:11 +0200

    proper variable handling, added remote shell TODO

--- debug_terminal_wrapper.sh
+++ debug_terminal_wrapper.sh
@@ -6,6 +6,15 @@
 # $3="-c"
 # $4=cd <PATH> && exec $SHELL
 
+#TODO shell on remote Server
+#command run for start shell on remote server
+#printing 0: /usr/bin/xterm
+#printing 1: -e
+#printing 2: /bin/sh
+#printing 3: -c
+#printing 4: ssh XXXX -t 'cd '\''/root'\'' && exec $SHELL -'
+
+
 # set here your prefered terminal emulator
 TERM="/usr/bin/termite"
 # exec option by your terminal
@@ -22,8 +31,8 @@ echo "printing 4: $4"
 # Get the directory path of parameter 4
 # Modified regex from https://www.linuxquestions.org/questions/programming-9/bash-grep-regex-validation-for-path-names-926900/#post4590415
 # Thanks to Dr_P_Ross
-DIRECTORY=`echo $4 | grep -o -E -e '/[-_A-Za-z0-9:]+(/[-_A-Za-z0-9:]*)*'`
+DIRECTORY=$(echo $4 | grep -o -E -e '/[-_A-Za-z0-9]+(/[-_A-Za-z0-9:=.]*)*')
 echo "following dir found: $DIRECTORY"
-echo "running following command: $TERM $1 $SHELL $DIROPTION $DIRECTORY"
+echo "running following command: "${TERM}" "${EXECOPTION}" "${SHELL}" "${DIROPTION}" "${DIRECTORY}""
 
-$TERM $1 $SHELL $DIROPTION $DIRECTORY
+#"${TERM}" "${EXECOPTION}" "${SHELL}" "${DIROPTION}" "${DIRECTORY}"
